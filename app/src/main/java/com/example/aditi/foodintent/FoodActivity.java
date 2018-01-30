ab4ae6071f5a6e90f5ad80a14ba1ce51a7173453
aditi.shevale999@gmail.com <aditi.shevale999@gmail.com>
2018-01-30 11:47:52 +0530

    Implicit Code Added

--- app/src/main/java/com/example/aditi/foodintent/FoodActivity.java
+++ app/src/main/java/com/example/aditi/foodintent/FoodActivity.java
@@ -2,12 +2,24 @@ package com.example.aditi.foodintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FoodActivity extends AppCompatActivity {
+TextView txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
-    }
+        txt1 =(TextView)findViewById(R.id.result);
+
+//        To get Data from Intent
+
+       Bundle b1 = getIntent().getExtras();
+//       Access data
+       String s1 = b1.getString("name");
+
+//       Set S1 to TextView
+        txt1.setText(s1);
+  }
 }
--- app/src/main/java/com/example/aditi/foodintent/MainActivity.java
+++ app/src/main/java/com/example/aditi/foodintent/MainActivity.java
@@ -4,19 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText e1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        e1 =(EditText)findViewById(R.id.editText);
     }
 
     public void Click(View view) {
         Intent intent = new Intent(this,FoodActivity.class);
+        intent.putExtra("Name",e1.getText().toString());
         startActivity(intent);
     }
 }
